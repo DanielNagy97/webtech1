f52505d392e081fbaba7c183d8db2fdb24d80091
danielnagy97 <nagydaniel.z97@gmail.com>
2019-11-02 20:57:26 +0100

    Assignment finished

--- js/index.js
+++ js/index.js
@@ -10,41 +10,45 @@ $(document).ready(function () {
                     $("#content").load($(this).find('a').attr("href"));
                 }
                 else {
-                    open("index.html","_self")
+                    open("index.html","_self");
                 }
-
-            })
-        })
+            });
+        });
 });
 
 function openCar(manufacturer){
     document.cookie="name=" + manufacturer;
     $.getJSON("manufacturer", function(data){
+        if(data.length > 0) {
+            var table = $('<table id="selectedCarsTable"></table>');
+            $(table).append('<tr><th>Name</th><th>Consumption</th><th>Color</th><th>Manufacturer</th><th>Available</th><th>Year</th><th>Horsepower</th></tr>');
+            $.each(data, function (key, value) {
+                var row = $('<tr></tr>');
+                var nameCell = $('<td>' + value.name + '</td>');
+                var consumptionCell = $('<td>' + value.consumption + '</td>');
+                var colorCell = $('<td>' + value.color + '</td>');
+                var manufacturerCell = $('<td>' + value.manufacturer + '</td>');
+                var availableCell = $('<td>' + value.available + '</td>');
+                var yearCell = $('<td>' + value.year + '</td>');
+                var horsepowerCell = $('<td>' + value.horsepower + '</td>');
 
-        var table = $('<table id="carsTable"></table>');
-        $(table).append('<tr><th>Name</th><th>Consumption</th><th>Color</th><th>Manufacturer</th><th>Available</th><th>Year</th><th>Horsepower</th></tr>');
-
-        $.each(data, function(key, value){
-            var row = $('<tr></tr>');
-            var nameCell = $('<td>' + value.name + '</td>');
-            var consumptionCell = $('<td>' + value.consumption + '</td>');
-            var colorCell = $('<td>' + value.color + '</td>');
-            var manufacturerCell = $('<td>' + value.manufacturer + '</td>');
-            var availableCell = $('<td>' + value.available + '</td>');
-            var yearCell = $('<td>' + value.year + '</td>');
-            var horsepowerCell = $('<td>' + value.horsepower + '</td>');
-
-            $(row).append(nameCell);
-            $(row).append(consumptionCell);
-            $(row).append(colorCell);
-            $(row).append(manufacturerCell);
-            $(row).append(availableCell);
-            $(row).append(yearCell);
-            $(row).append(horsepowerCell);
-            $(table).append(row);
-        })
-        $("#content").append(table);
-
+                $(row).append(nameCell);
+                $(row).append(consumptionCell);
+                $(row).append(colorCell);
+                $(row).append(manufacturerCell);
+                $(row).append(availableCell);
+                $(row).append(yearCell);
+                $(row).append(horsepowerCell);
+                $(table).append(row);
+            })
+            $("#selectedCars").empty();
+            $("#selectedCars").append('<h2>' + manufacturer + ' car(s): </h2>');
+            $("#selectedCars").append(table);
+        }
+        else{
+            $("#selectedCars").empty();
+            $("#selectedCars").append('<h2>There are no ' + manufacturer + ' cars: </h2>');
+        }
     }).error(function(data){
         console.log(data);
     })
@@ -62,9 +66,8 @@ function showSlides(n){
         $(".pbText .name").html(data[n].name);
         $(".pbText .manufacturer").html(data[n].manufacturer);
         $(".pbText .consumption").html(data[n].consumption);
-        $(".viewAll").html("View all "+data[n].manufacturer+" >>");
         $("#carHome").attr("src","images/car"+n+".png");
-    })
+    });
 }
 
 function showForm(form) {
